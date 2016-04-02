1a7beb168f58dc879a7bb0f5d35f965d3e3ab919
Raphael Michel <mail@raphaelmichel.de>
2016-04-02 23:26:56 +0200

    Added a test for propagation of deletion

--- functional_tests/test_cluster.py
+++ functional_tests/test_cluster.py
@@ -4,7 +4,7 @@ import time
 from functional_tests.utils import running_cockatiel_cluster
 
 
-def test_put_propagagion():
+def test_put_propagation():
     with running_cockatiel_cluster() as ports:
         content = 'Hello, this is a testfile'.encode('utf-8')
         resp = requests.put(
@@ -22,3 +22,36 @@ def test_put_propagagion():
         )
         assert resp.content == content
         assert resp.headers['Content-Type'] == 'text/plain'
+
+
+def test_delete_propagation():
+    with running_cockatiel_cluster() as ports:
+        content = 'Hello, this is a testfile'.encode('utf-8')
+        resp = requests.put(
+            'http://127.0.0.1:{port}{path}'.format(path='/foo/bar.txt', port=ports[0]),
+            content
+        )
+        assert resp.status_code == 201
+        path = resp.headers['Location']
+
+        time.sleep(2)
+
+        resp = requests.get(
+            'http://127.0.0.1:{port}{path}'.format(path=path, port=ports[1]),
+            content
+        )
+        assert resp.status_code == 200
+        assert resp.content == content
+        assert resp.headers['Content-Type'] == 'text/plain'
+
+        resp = requests.delete(
+            'http://127.0.0.1:{port}{path}'.format(path=path, port=ports[1]),
+        )
+        assert resp.status_code == 200
+
+        time.sleep(2)
+
+        resp = requests.delete(
+            'http://127.0.0.1:{port}{path}'.format(path=path, port=ports[0]),
+        )
+        assert resp.status_code == 404
